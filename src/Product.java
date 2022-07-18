dc2d2a7e61f80297255754e176653ed995488092
NageshN9901 <nagesh.n8991@gmail.com>
2022-07-18 20:40:40 +0530

    ed

---
+++ src/Product.java
@@ -0,0 +1,17 @@
+
+public class Product {
+	string type;
+	int quantity;
+	double price;
+	
+	Product (string t,int q,quantity p){
+		public static void main (string[]args) {
+			Product p1 =new /product ("t-shirt",2,3000);
+			Product p2 = new Product("nail-polish",5,2000);
+			System.out.println(p1,quantity+" "+p1.type+" "+p1.price);
+			System.out.println();
+			System.out.println(p2 quantity+" "+p2.type+" "+p2.price);
+		}
+	}
+		
+	
